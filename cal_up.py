63825f7adbd7d3361d04c75943fd3988794cf8c6
baakor <sendogmatic@gmail.com>
2026-07-12 20:12:45 +0100

    restructured the cal_up.py, concerning the response when a job is finished and the likes, also removed unwanted files, the hello.py and the cal.py

--- cal_up.py
+++ cal_up.py
@@ -1,5 +1,8 @@
+import numpy as np
+
 def calculator():
-    print("insert values to calculate")
+    print("insert values to calculate\n# = square root\n$ for = square\n^ = power\n+ = addition\n- = subtraction\n* = multiplication\n/ = division"
+          )
 
     x = input()
     xseperated = ""
@@ -17,27 +20,47 @@ def calculator():
     if xseperated != "":
         y.append(float(xseperated))
 
-    print(y)
-    print(len(y))
+    # print(y)
+    # print(len(y))
 
     result = y[0]
     i = 1
     while i < len(y):
         op = y[i]
-        next_num = y[i + 1]
-        if op == "+":
-            result = result + next_num
-        elif op == "-":
-            result = result - next_num
-        elif op == "*":
-            result = result * next_num
-        elif op == "/":
-            if next_num == 0:
-                result = "cannot divide by zero"
+        if op == "#":
+            if result <= 0:
+                result = "error: cannot take square root of negative number"
                 break
-            result = result / next_num
-        i += 2
-    print(result)
-    calculator()
-
+            result = np.sqrt(result)
+            i += 1
+        elif op == "$":
+            result = result * result
+            i += 1
+        # elif op == "!":
+            #CONTINUE FROM HERE TO INTEGRATE FACTORIAL CALCULATIONS AND FIBONACCI CALCULATIONS
+        else:
+            next_num = y[i + 1]
+            if op == "+":
+                result = result + next_num
+            elif op == "-":
+                result = result - next_num
+            elif op == "*":
+                result = result * next_num
+            elif op == "^":
+                result = result ** next_num
+            elif op == "/":
+                if next_num == 0:
+                    result = "cannot divide by zero"
+                    break
+                result = result / next_num
+            i += 2
+    print(result, "\n")
+    recalculate = input("Do you want to perform another calculation? (yes/no): ")
+    while recalculate.lower() == "yes":
+        calculator()
+        if recalculate.lower() == "no":
+            print("Thank you for using the calculator!")
+    else:
+        if recalculate.lower() != "no" and recalculate.lower() != "yes":
+            print("Invalid input. Exiting the calculator.")
 calculator()
--- hello.py
+++
@@ -1,4 +0,0 @@
-b = """brighter comes in the morning,
-    while for the afternoon the sun is very dark,
-    evening has a way of making me love"""
-print("love" in b)
